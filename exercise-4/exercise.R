400ab79b566deb4a23fab2fc0842484665a13b22
sperng <sam.perng@live.com>
2017-04-20 09:33:30 -0700

    challenge unfinished

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,33 +1,38 @@
 # Exercise 4: DPLYR and flights data
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+#install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
-
+#View(flights)
+nrow(flights)
+ncol(flights)
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights <- mutate(flights,gain = arr_delay - dep_delay)
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, gain)
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- flights %>% mutate(gain = arr_delay - dep_delay) %>% 
+                                arrange(gain)
 
 # Make a histogram of the amount of gain using the `hist` command
+hist(flights$gain)
 
 
 # On average, did flights gain or lose time?
-
+mean(flights$gain, na.rm = TRUE)
 
 # Create a data.frame that is of flights headed to seatac ('SEA'), 
+sea.frame <- flights %>% select(gain, dest) %>% filter(dest == 'SEA')
 
 
 # On average, did flights to seatac gain or loose time?
+mean(sea.frame$gain, na.rm =  TRUE)
 
 ### Bonus ###
 # Write a function that allows you to specify an origin, a destination, and a column of interest
